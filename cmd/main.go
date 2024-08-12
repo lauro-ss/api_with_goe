0caff32355dd12eace917f768103f3225f7f1581
Lauro Santana <devlauross@gmail.com>
2024-08-12 00:23:23 -0300

    feat: add gracefully shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,15 @@
 package main
 
-import "net/http"
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
 func main() {
 	mux := http.NewServeMux()
@@ -9,5 +18,31 @@ func main() {
 		w.Write([]byte("Hello World"))
 	})
 
-	http.ListenAndServe(":8080", mux)
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	go func() {
+		log.Println("Server is running on 8080")
+		err := server.ListenAndServe()
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatal(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+	<-c
+	log.Println("Closing the server...")
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal(err)
+	}
+
+	<-ctx.Done()
+	log.Println("Server gracefully shutdown")
+
 }
